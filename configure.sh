2675c40ca71be3597cf6ccbe7d8b550284b1f9b8
pam196 <phil@ecr.co.uk>
2016-08-22 16:06:04 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -8,21 +8,21 @@ locale-gen en_US.UTF-8
 dpkg-reconfigure locales 
 
 #apache redirect root
-echo "
-ServerName localhost
-<Location />
-	RedirectMatch 301 ^/$ /bareos-webui/
-</Location>
-" >/etc/apache2/conf-available/bareos-redirect.conf
-ln -s /etc/apache2/conf-available/bareos-redirect.conf /etc/apache2/conf-enabled/bareos-redirect.conf
+#echo "
+#ServerName localhost
+#<Location />
+#	RedirectMatch 301 ^/$ /bareos-webui/
+#</Location>
+#" >/etc/apache2/conf-available/bareos-redirect.conf
+#ln -s /etc/apache2/conf-available/bareos-redirect.conf /etc/apache2/conf-enabled/bareos-redirect.conf
 
-#fix permissions
-chown -R bareos:bareos /var/log/bareos /backup /etc/bareos
-chown -R www-data:www-data /var/log/apache2
-chown -R mysql:adm /db /var/log/mysql
+##fix permissions
+chown -R bareos:bareos /var/log/bareos /etc/bareos
+#chown -R www-data:www-data /var/log/apache2
+#chown -R mysql:adm /db /var/log/mysql
 
 #enable services
-for s in bareos-dir bareos-sd bareos-fd postfix apache2 mysql; do
+for s in bareos-fd ; do
 	update-rc.d $s defaults &&  update-rc.d $s enable
 done
 
